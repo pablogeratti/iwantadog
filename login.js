8d039e18c3cd6454e6517e8923986633e2f37adb
pablosqa <pablogeratti73@gmail.com>
2023-10-04 19:29:33 -0300

    show de bola

--- login.js
+++ login.js
@@ -1,3 +1,11 @@
 "trabalhando no login"
 
 "finalizando sistema login"
+
+//testando stash
+//testando stash
+//testando stash
+//testando stash
+//testando stash
+//testando stash
+//testando stash
